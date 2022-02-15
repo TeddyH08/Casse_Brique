e4e1e825fdab1fe30f339f74cd23389f2fa1e633
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-15 14:24:56 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,17 @@ var dy = -2;
 
 var ballRadius = 10;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
 /* Création de la balle */
 
 function drawBall() {
@@ -29,7 +40,36 @@ function draw() {
     if(y + dy > canvas.height-ballRadius || y + dy < 0) {
         dy = -dy;
     }
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
     x += dx;
     y += dy;
+}
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
 }
   setInterval(draw, 10);
