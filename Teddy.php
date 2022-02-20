bd0662db7a4b63405a013e16666feb795c3be1fe
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-20 19:02:20 +0100

    Create Teddy.php

---
+++ Teddy.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niveau Teddy</title>
+    <link rel="stylesheet" href="assets/css/Teddy.css">
+</head>
+
+<body>
+    <h1>Petit Memory</h1>
+    <div class="row">
+        <div data-color="johann" class="card johann color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="axel" class="card axel color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="guillaume" class="card guillaume color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="jjj" class="card jjj color-hidden" onclick="onCardClicked(event)"></div>
+    </div>
+
+    <div class="row">
+        <div data-color="romain1" class="card romain1 color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="axel" class="card axel color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="jordan" class="card jordan color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="romain2" class="card romain2 color-hidden" onclick="onCardClicked(event)"></div>
+    </div>
+
+    <div class="row">
+        <div data-color="jjj" class="card jjj color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="romain2" class="card romain2 color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="steven" class="card steven color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="johann" class="card johann color-hidden" onclick="onCardClicked(event)"></div>
+    </div>
+
+    <div class="row">
+        <div data-color="steven" class="card steven color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="guillaume" class="card guillaume color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="romain1" class="card romain1 color-hidden" onclick="onCardClicked(event)"></div>
+        <div data-color="jordan" class="card jordan color-hidden" onclick="onCardClicked(event)"></div>
+    </div>
+
+    <script src="Teddy.js"></script>
+</body>
+</html>
