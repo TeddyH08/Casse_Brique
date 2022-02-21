7c671c5e31028f924914e5db002be3ca02785797
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-21 10:55:24 +0100

    Update Quentin.php

--- Quentin.php
+++ Quentin.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
+<body style="background:#333333">
+
+    
+
+    <audio src="assets/sound/dort-mystic.mp3" autoplay>
+        Votre navigateur ne supporte pas l'élément <code>audio</code>.
+      </audio>
+
+    <div id="point" style="color:white;font-size:25px;float:right;margin-right:10%;background:black;width:40px;text-align:center;">3</div>
+    
+        <div id="ctn" >
+            <canvas id="canvas" class="canvas" width="590" height="985"></canvas>
+    
+    
+            <div id="babale"></div>
+    
+            <div id="cucure"></div>
+    
+        </div>  
+        <script src="main.js"></script>
+        <script src="canvas.js"></script>
+</body>
+</html>
