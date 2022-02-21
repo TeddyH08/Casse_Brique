ee3bc3772fafc6b9c57ce7bb51b5cb7a81fd36d7
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-21 09:22:09 +0100

    Create Nina.php

---
+++ Nina.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="nina.css" />
+    <title>test</title>
+</head>
+<body>
+<h1>Le carré</h1>
+<p>but du jeu: mettre les bords du carré avec les images en clickant sur les différentes boites.<br> 
+    On peut clicker sur les images on a déjà clické.<br> 
+    Il n'y a qu'une seule série de click qui fonctionne et elle est composée de 4 boites distinctes.</p>
+
+<script src="nina.js"></script>
+</body>
+</html>
