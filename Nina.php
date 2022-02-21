a4ffa30466438833356f1633ad5733b15a8680e3
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-21 09:31:14 +0100

    Update Nina.php

--- Nina.php
+++ Nina.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/nina.css" />
     <title>test</title>
 </head>
 <body>
     <div class="niveau">
         <a href="index.php">Niveau TLM</a>
         <a href="index.php">Niveau Quentin</a>
-        <a href="index.php">Niveau Maria</a>
+        <a href="Nina.php">Niveau Maria</a>
         <a href="index.php">Niveau Andy</a>
         <a href="Teddy.php">Niveau Teddy</a>
     </div>
