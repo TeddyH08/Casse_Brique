7a2145f3663f414ecc4c868d959990fafdc06fc7
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-21 09:23:10 +0100

    Update Nina.php

--- Nina.php
+++ Nina.php
@@ -4,15 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nina.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
     <title>test</title>
 </head>
 <body>
-<h1>Le carré</h1>
-<p>but du jeu: mettre les bords du carré avec les images en clickant sur les différentes boites.<br> 
+    <div class="niveau">
+        <a href="index.php">Niveau TLM</a>
+        <a href="index.php">Niveau Quentin</a>
+        <a href="index.php">Niveau Maria</a>
+        <a href="index.php">Niveau Andy</a>
+        <a href="Teddy.php">Niveau Teddy</a>
+    </div>
+    
+    <h1>Le carré</h1>
+    <p>but du jeu: mettre les bords du carré avec les images en clickant sur les différentes boites.<br> 
     On peut clicker sur les images on a déjà clické.<br> 
     Il n'y a qu'une seule série de click qui fonctionne et elle est composée de 4 boites distinctes.</p>
 
-<script src="nina.js"></script>
+    <script src="assets/js/nina.js"></script>
 </body>
 </html>
