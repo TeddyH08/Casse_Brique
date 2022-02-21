9c12c061453e97a65bb687284902973476ab3803
Teddy <96143755+TeddyH08@users.noreply.github.com>
2022-02-21 10:56:46 +0100

    Update Quentin.php

--- Quentin.php
+++ Quentin.php
@@ -5,11 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/quentin.css">
 </head>
 
 <body style="background:#333333">
-
+    
+    <div class="niveau">
+        <a href="index.php">Niveau TLM</a>
+        <a href="Quentin.php">Niveau Quentin</a>
+        <a href="Nina.php">Niveau Maria</a>
+        <a href="Andy.php">Niveau Andy</a>
+        <a href="Teddy.php">Niveau Teddy</a>
+    </div>
     
 
     <audio src="assets/sound/dort-mystic.mp3" autoplay>
@@ -27,7 +34,7 @@
             <div id="cucure"></div>
     
         </div>  
-        <script src="main.js"></script>
-        <script src="canvas.js"></script>
+        <script src="assets/js/quentinm.js"></script>
+        <script src="assets/js/quentinc.js"></script>
 </body>
 </html>
